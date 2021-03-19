ae2e3f75921cfd50a19b2bb453a241c464452af3
ammad <ammadsid87@gmail.com>
2021-03-19 20:17:21 +0530

    deploy setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(name: "Admin", email: "admin@example.com",role: 0, password: "password") unless User.find_by(role: 0)
+GeneralPanel.create(tax_ref: 5, exch_rate: 54, local_transportation_cost: 2, exp: 1)
