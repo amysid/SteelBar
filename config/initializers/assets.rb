bb5879084b811977c85cf9fd3a11420340d15f2c
Nisha Deol <nisha.deol@proprofs.com>
2021-04-03 11:50:10 +0530

    validation on all forms

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( ckeditor/*)
-Rails.application.config.assets.precompile += %w( admin/admin_application.js admin/admin_application.scss )
+Rails.application.config.assets.precompile += %w( admin/admin_application.js customer_panel.js admin/admin_application.scss )
